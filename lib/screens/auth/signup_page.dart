ba7dacf28b51ff346a3e5410aafc6ef198da03ea
Lans12345 <olanalans12345@gmail.com>
2024-05-11 08:11:57 +0800

    added auth ui

--- lib/screens/auth/signup_page.dart
+++ lib/screens/auth/signup_page.dart
@@ -1,18 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:pa/screens/auth/signup_page.dart';
 import 'package:pa/screens/home_screen.dart';
 import 'package:pa/widgets/button_widget.dart';
 import 'package:pa/widgets/text_widget.dart';
 import 'package:pa/widgets/textfield_widget.dart';
 
-class LoginPage extends StatefulWidget {
-  const LoginPage({super.key});
+class SignupPage extends StatefulWidget {
+  const SignupPage({super.key});
 
   @override
-  State<LoginPage> createState() => _LoginPageState();
+  State<SignupPage> createState() => _SignupPageState();
 }
 
-class _LoginPageState extends State<LoginPage> {
+class _SignupPageState extends State<SignupPage> {
   final email = TextEditingController();
   final password = TextEditingController();
   @override
@@ -38,6 +37,14 @@ class _LoginPageState extends State<LoginPage> {
             const SizedBox(
               height: 10,
             ),
+            TextWidget(
+              text: 'Create Account',
+              fontSize: 18,
+              fontFamily: 'Medium',
+            ),
+            const SizedBox(
+              height: 10,
+            ),
             TextFieldWidget(
               controller: email,
               label: 'Email',
@@ -45,54 +52,22 @@ class _LoginPageState extends State<LoginPage> {
             const SizedBox(
               height: 10,
             ),
-            Column(
-              children: [
-                TextFieldWidget(
-                  showEye: true,
-                  isObscure: true,
-                  controller: password,
-                  label: 'Password',
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(right: 50),
-                  child: Align(
-                    alignment: Alignment.bottomRight,
-                    child: TextButton(
-                      onPressed: () {},
-                      child: TextWidget(
-                        text: 'Forgot Password?',
-                        fontFamily: 'Bold',
-                        fontSize: 12,
-                      ),
-                    ),
-                  ),
-                ),
-              ],
+            TextFieldWidget(
+              showEye: true,
+              isObscure: true,
+              controller: password,
+              label: 'Password',
             ),
             const SizedBox(
               height: 30,
             ),
             ButtonWidget(
               width: 200,
-              label: 'Login',
+              label: 'Signup',
               onPressed: () {
                 Navigator.of(context).pushReplacement(MaterialPageRoute(
                     builder: (context) => const HomeScreen()));
               },
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).push(MaterialPageRoute(
-                    builder: (context) => const SignupPage()));
-              },
-              child: TextWidget(
-                text: 'Signup',
-                fontFamily: 'Bold',
-                fontSize: 18,
-              ),
             ),
           ],
         ),
